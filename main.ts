4e06c2dc15f737dcec119924fb3c3f1e2f42821c
Leon Jäkel <leon1jaekel@aol.com>
2022-09-22 14:27:13 +0200

    Add typescript support with tsconfig
    - target: ESNext
    - active declaration
    - active sourceMap

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
-const robot = require("robotjs");
+import { getMousePos, moveMouse } from 'robotjs';
 
-const MOVE_MARGIN = 10;
-var direction = "down";
+const MOVE_MARGIN: number = 10;
+let direction: string = "down";
 
-var mouse = robot.getMousePos();
-var x = mouse.x;
-var y = mouse.y;
+let mouse: { x: number, y: number } = getMousePos();
+let x = mouse.x;
+let y = mouse.y;
 
 const handle = () => {
-    var mouse = robot.getMousePos();
+    mouse = getMousePos();
     console.log(mouse.x + "|" + mouse.y)
     console.log(x + "|" + y)
 
@@ -18,16 +18,16 @@ const handle = () => {
         console.log("mouse position did not change")
 
         if (direction === "down") {
-            robot.moveMouse(mouse.x + MOVE_MARGIN, mouse.y + MOVE_MARGIN);
+            moveMouse(mouse.x + MOVE_MARGIN, mouse.y + MOVE_MARGIN);
             direction = "up";
         } else {
-            robot.moveMouse(mouse.x - MOVE_MARGIN, mouse.y - MOVE_MARGIN);
+            moveMouse(mouse.x - MOVE_MARGIN, mouse.y - MOVE_MARGIN);
             direction = "down";
         }
         console.log("new mouse position: " + x + "|" + y)
     }
 
-    var mouse = robot.getMousePos();
+    mouse = getMousePos();
     x = mouse.x;
     y = mouse.y;
     console.log("-------------------------------------")

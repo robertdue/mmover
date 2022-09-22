9fcb21e948361176f4167e4357d71cd751dddf01
Leon Jäkel <leon1jaekel@aol.com>
2022-09-22 22:07:43 +0200

    Add and configure dotenv to externalize configuration properties

--- src/Types.ts
+++ src/Types.ts
@@ -1,4 +1,6 @@
 export const TYPES = {
     Worker: Symbol.for('Worker'),
     MousePosition: Symbol.for('MousePosition'),
+    Timeout: Symbol.for('Timeout'),
+    MoveMargin: Symbol.for('MoveMargin'),
 }
